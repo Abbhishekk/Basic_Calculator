813961095ccc4d9d46e61c18f30ac53a600417e8
Abhishek Bante <abhishek.bante.cse@ghrce.raisoni.net>
2023-04-20 11:43:34 +0530

    multiple operand functionality added

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
+var a = document.getElementById('demo')
+var b = document.getElementById('text')
+window.onload= ()=>{
+    a.value= null;
+    b.value = null;
+}
 function fun1( v) {
-    var a,b;
-    a=document.getElementById(v).value;
-
-    b=document.getElementById('demo1');
-    b.value=b.value+a;
+    var x;
+    x=document.getElementById(v).value;
+    a.value=a.value+x;
 }
 function getstring(){
-    var a,i,n1,op,n2;
-    i=0;
-    n1="";
-    n2="";
-    op="";
-    a=document.getElementById('demo1').value;
     
-    if(a[0]=='.')
-    {
-        a='0'+a;
-    }
-    while(i<a.length)
-    {
-        if(a.charAt(i)!='+' && a.charAt(i)!='-' && a.charAt(i)!='*' && a.charAt(i)!='/')
-        {n1=n1+a[i];
-        i=i+1;}
-        else{
-            break;
+    var result;
+    try {
+        result = eval(a.value);
+        if(Number.isInteger(result)){
+            b.value = result;
         }
+        else{
+            b.value = result.toFixed(2);
+        }    
+    } catch (error) {
+        alert("Invalid number");
     }
-    n2=a.slice(i+1,a.length);
-
-    if(a[i]=='+')
-        document.getElementById('demo').value=Number(n1)+Number(n2);
-    else if(a[i]=='-')
-        document.getElementById('demo').value=Number(n1)-Number(n2);
-    else if(a[i]=='*')
-        document.getElementById('demo').value=Number(n1)*Number(n2);
-    else if(a[i]=='/')
-        document.getElementById('demo').value=Number(n1)/Number(n2);
+    
+    console.log(result);
+   
 
 
 }
 function reset() {
-    document.getElementById('demo1').value=null;
+    a.value=null;
+    b.value = null;
 }
